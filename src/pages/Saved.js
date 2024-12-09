52eca953200db1769fa77598e723c92ae7a6ea33
zunarvy19 <arvy977#gmail.com>
2024-12-09 17:25:24 +0700

    style(css): optimized saved page

--- src/pages/Saved.js
+++ src/pages/Saved.js
@@ -4,28 +4,60 @@ import { removeArticle } from "../redux/features/savedSlice";
 
 function Saved() {
   const savedArticles = useSelector((state) => state.saved.savedArticles);
+  let number = 0;
   const dispatch = useDispatch();
 
   console.log(savedArticles);
 
   return (
     <div className="p-5">
-      <h1 className="text-2xl font-bold mb-4">Saved News</h1>
-      <ul>
-        {savedArticles.map((article, index) => (
-          <li key={index} className="mb-4">
-            <a href={article.web_url} target="_blank" rel="noopener noreferrer">
-              {article.abstract || "No Title Available"}
-            </a>
-            <button
-              onClick={() => dispatch(removeArticle(article))}
-              className="ml-4 bg-red-500 text-white px-4 py-2 rounded"
-            >
-              Un-Save
-            </button>
-          </li>
-        ))}
-      </ul>
+      <h1 className="text-3xl font-bold my-6 text-center">Saved News</h1>
+
+      <div className="w-[89%] flex justify-center mx-auto">
+        <hr className="border border-gray-200 border-t-2 mb-6"></hr>
+        <div className="overflow-x-auto ">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Abstract</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {savedArticles.map((article, index) => (
+                <tr key={index}>
+                  <th>{++number}</th>
+                  <td>{article.headline.main}</td>
+                  <td>{article.abstract}</td>
+                  <td>
+                    <ul className="flex flex-row gap-x-5">
+                      <li className="">
+                        <a
+                          className="btn "
+                          href={article.web_url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Read
+                        </a>
+                      </li>
+                      <li
+                        className="btn btn-warning"
+                        onClick={() => dispatch(removeArticle(article))}
+                      >
+                        Delete
+                      </li>
+                    </ul>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -53,7 +53,7 @@ function Search() {
           />
           <button
             type="submit"
-            className="bg-[#222831] text-white px-4 py-2 rounded ml-2"
+            className="bg-[#222831] text-white px-4 py-2 rounded ml-2 btn text-lg hover:bg-[#222831]"
           >
             Search
           </button>
