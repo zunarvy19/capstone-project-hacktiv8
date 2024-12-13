b24b10e59f73158d197f4c2e33cb653a1155827a
zunarvy19 <arvy977#gmail.com>
2024-12-13 23:01:29 +0700

    style(modal): add modal toast

--- src/pages/Saved.js
+++ src/pages/Saved.js
@@ -38,7 +38,7 @@ function Saved() {
                       <ul className="flex flex-row gap-x-5">
                         <li className="">
                           <a
-                            className="btn "
+                            className="btn btn-outline"
                             href={article.web_url}
                             target="_blank"
                             rel="noopener noreferrer"
@@ -46,11 +46,42 @@ function Saved() {
                             Read
                           </a>
                         </li>
-                        <li
-                          className="btn btn-warning"
-                          onClick={() => dispatch(removeArticle(article))}
-                        >
-                          Delete
+                        <li>
+                          <button
+                            className="btn btn-error text-white"
+                            onClick={() =>
+                              document.getElementById("my_modal_1").showModal()
+                            }
+                          >
+                            Delete
+                          </button>
+                          <dialog id="my_modal_1" className="modal">
+                            <div className="modal-box">
+                              <h3 className="font-bold text-lg">
+                                Are you sure?
+                              </h3>
+                              <p className="py-4">
+                                This action cannot be{" "}
+                                <span className=" font-semibold">undone! </span>
+                                Please confirm your choice below.
+                              </p>
+                              <div className="modal-action">
+                                <form method="dialog" className="mr-2">
+                                  <button className="btn border border-red-500 text-white bg-red-500 hover:bg-red-500 rounded">
+                                    Cancel
+                                  </button>
+                                  <button
+                                    className="btn ml-2 border border-black rounded hover:bg-white hover:border-black"
+                                    onClick={() => {
+                                      dispatch(removeArticle(article));
+                                    }}
+                                  >
+                                    Confirm
+                                  </button>
+                                </form>
+                              </div>
+                            </div>
+                          </dialog>
                         </li>
                       </ul>
                     </td>
